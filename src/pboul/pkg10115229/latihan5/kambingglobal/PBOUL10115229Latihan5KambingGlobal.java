c241aa935cfc54243d426d8d660e92bd4e473a32
fakhri ramdani <muhammadlukman09@gmail.com>
2019-09-26 09:52:53 +0700

    project kelima

---
+++ src/pboul/pkg10115229/latihan5/kambingglobal/PBOUL10115229Latihan5KambingGlobal.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan5.kambingglobal;
+
+/**
+ *
+ * @author Lukman
+ * NIM          : 10115229
+ * NAMA         : MUHAMMAD LUKMAN HAKIM
+ * HARITANGGAL  : SABTU, 21 SEPTEMBER 2019
+ * LOG
+ */
+public class PBOUL10115229Latihan5KambingGlobal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Variabel jumlahkambung menjadi variabel instance
+        
+        int jumlahKambing = 88;
+        
+        //Method untuk menampilkan jumlah kambing
+        public void getJumlahKambing(){
+            System.out.println("Jumlah kambing: " + jumlahKambing);
+        }
+        
+        public void tambahKambing(){
+            jumlahKambing = jumlahKambing + 5;
+            System.out.println("Jumlah kambing setelah ditambah: " + jumlahKambing);
+            
+    public static void main(String [] args){
+        KambingGlobal kambingSusu = new KambingGlobal();
+        
+        // Menampilkan jumlah kambing yang ada saat program pertama x berjalan
+    }   
+    kambingSusu.tambahKambing();
+    
+    // Menambah satu kambing
+    kambingSusu.tambahKambing();
+    
+    // Menampilkan jumlah kambing yang ada
+    kambingSusu.getJumlahKambing();
+    
+            
+        })
+    }
+    
+}
